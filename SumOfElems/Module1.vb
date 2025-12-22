ec586967230b6932ebaabb8f681aa4877efbd4de
Alexander <pchp250g8@tutamail.com>
2025-12-22 11:45:39 +0300

    Добавьте файлы проекта.

---
+++ SumOfElems/Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim nElems = 0
+        Console.Clear()
+        Console.WriteLine("Input a count of elements")
+        Int32.TryParse(Console.ReadLine(), nElems)
+        Dim iArray(nElems) As Integer
+        Dim oRnd = New Random()
+        For i = 0 To nElems - 1
+            Dim nItem = oRnd.Next(1, 50)
+            iArray(i) = nItem
+        Next
+        Dim nSum = 0
+        For i = 0 To nElems - 1
+            nSum += iArray(i)
+            Console.Write(Str(iArray(i)) + " ")
+        Next
+        Console.WriteLine(vbCrLf + $"The Sum of {nElems} elements of the array is:{nSum}")
+        Console.Read()
+    End Sub
+
+End Module
---
+++ SumOfElems/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Общие сведения об этой сборке предоставляются следующим набором
+' набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+' связанные со сборкой.
+
+' Проверьте значения атрибутов сборки
+
+<Assembly: AssemblyTitle("SumOfElems")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("SumOfElems")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+<Assembly: Guid("71dd8af4-a97a-478b-8936-03c40b2f4570")>
+
+' Сведения о версии сборки состоят из следующих четырех значений:
+'
+'      Основной номер версии
+'      Дополнительный номер версии
+'   Номер сборки
+'      Редакция
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
